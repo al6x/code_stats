c95edc9fe60882e670d2b674448e4339f6f9474a
alexeypetrushin <alexey.petrushin@gmail.com>
2011-08-11 10:12:37 +0400

    upd dependencies

--- lib/code_stats/gems.rb
+++ lib/code_stats/gems.rb
@@ -1,6 +1,6 @@
 if respond_to? :fake_gem  
-  fake_gem 'ruby_ext', '0.4.23'
-  fake_gem 'vfs', '0.3.9'
-  fake_gem 'haml', '3.1'
-  fake_gem 'tilt', '1.3'
+  fake_gem 'ruby_ext', '~> 0.4'
+  fake_gem 'vfs',      '~> 0.3'
+  fake_gem 'haml',     '~> 3.1'
+  fake_gem 'tilt',     '~> 1.3'
 end
